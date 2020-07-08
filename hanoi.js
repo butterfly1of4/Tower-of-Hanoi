c21a8f2f6f1260d0f7f0e523953b6a57ab2cfc3e
butterfly1of4 <jhouck29@gmail.com>
2020-07-08 10:33:15 -0400

    event listeners

--- hanoi.js
+++ hanoi.js
@@ -5,8 +5,17 @@ const block3 = 3
 const block4 = 4
 
 let arrayA = []
-let arrayB = []
-let arrayC = []
+let arrayB = [ , , , ,]
+let arrayC = [ , , , ,]
+
+let poleA = document.querySelector(".poleA")
+let poleB = document.querySelector(".poleB")
+let poleC = document.querySelector(".poleC")
+
+let block1move = document.querySelector(".block1")
+let block2move = document.querySelector(".block2")
+let block3move = document.querySelector(".block3")
+
 
 console.log(block1, block2, block3, block4)
 
@@ -20,4 +29,25 @@ if (arrayA[x] > arrayA[y]) {
     console.log("not allowed")
 }
 block1 < block3
+console.log(arrayC)
 
+
+poleA.addEventListener("click", function (e){
+    console.log(arrayA)
+})
+poleB.addEventListener("click", function (e){
+    console.log(arrayB)
+})
+poleC.addEventListener("click", function (e){
+    console.log(arrayC)
+})
+
+block1move.addEventListener("click", function(e){
+    console.log(arrayA[0])
+})
+block2move.addEventListener("click", function(e){
+    console.log(arrayA[1])
+})
+block3move.addEventListener("click", function(e){
+    console.log(arrayA[2])
+})
